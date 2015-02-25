622411863f51943728867524b1fc02b542c332e6
ImInSpace <ignigomoreno@hotmail.com>
2015-02-25 17:13:19 +0100

    Projecte Unity

---
+++ Unity/Delta/Assets/color.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class color : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 translation = new Vector3();
+		translation.Set(0f,0f,0.01f);
+		transform.Translate(translation);
+	}
+}
