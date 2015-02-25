40691580a6850e7711d7a4d167555eb9700d19e8
jmigual <j.marce.igual@gmail.com>
2015-02-25 20:44:43 +0100

    Added many functions to AX12 class

--- Code/ax12.cpp
+++ Code/ax12.cpp
@@ -1,12 +1,78 @@
 #include "ax12.h"
 
-AX12::AX12(int ID, QObject *parent) : QObject(parent)
+AX12::AX12(dynamixel &dxl, int ID, QObject *parent) : 
+    QObject(parent),
+    _dxl(dxl),
+    _ID(ID)
 {
-    
+    if (_ID < 0) return;
+    _dxl.write_byte(_ID, RAM::TorqueEnable, true);
 }
 
 AX12::~AX12()
 {
     
+}
+
+double AX12::getCurrentLoad()
+{
+    int load = _dxl.read_word(_ID, RAM::PresentLoad);
+    load -= 1024;
+    if (load == -1024) load = 0;
+    return double((load/1023)*100);
+}
+
+double AX12::getCurrentPos()
+{
+    int pos = _dxl.read_word(_ID, RAM::PresentPosition);
+    return double((pos/1023)*300);  
+}
+
+int AX12::getCurrentTemp()
+{
+    int temp = _dxl.read_byte(_ID, RAM::PresentTemperature);
+    return temp;
+}
+
+double AX12::getCurrentSpeed()
+{
+    int speed = _dxl.read_word(_ID, RAM::PresentSpeed);
+    speed -= 1024;
+    if (speed == -1024) speed = 0;
+    return double((speed/1023)*100);
+}
+
+void AX12::setID(int ID)
+{
+    _ID = ID;
+    if (ID < 0) return;
+    _dxl.write_byte(_ID, RAM::TorqueEnable, true);
+}
+
+void AX12::setJointMode(bool mode)
+{
+    _mode = mode;
+    if (_mode) {
+        _dxl.write_word(_ID, ROM::CWAngleLimit, 0);
+        _dxl.write_word(_ID, ROM::CCWAngleLimit, 1023);
+    }
+    else {
+        _dxl.write_word(_ID, ROM::CWAngleLimit, 0);
+        _dxl.write_word(_ID, ROM::CCWAngleLimit, 0);
+    }
+}
+
+void AX12::setMinMax(double min, double max)
+{
+    if (min > max) std::swap(min, max);
+    
+    if (min < 0.0) min = 0;
+    if (max > 300.0) max = 300;
+    
+    min = (min/300)*1023;
+    max = (max/300)*1023;
+    
+    _dxl.write_word(_ID, ROM::CWAngleLimit, int (min));
+    _dxl.write_word(_ID, ROM::CCWAngleLimit, int (max));
 }
 
--- Code/ax12.h
+++ Code/ax12.h
@@ -2,13 +2,14 @@
 #define AX12_H
 
 #include <QObject>
+#include <algorithm>
 #include "dynamixel.h"
 
 #define P_TORQUE_ENABLE 24
 #define P_GOAL_POSITION 30
 #define P_PRESENT_POS	36
 
-
+/// @brief The AX12 class is used to control AX-12 motors from Dynamixel
 class AX12 : public QObject
 {
     Q_OBJECT
@@ -52,11 +53,61 @@ private:
         
     };    
     
+    /// @brief Refrence to a USB2Dynamixel controler
+    dynamixel &_dxl;
+    
+    /// @brief Stores the current ID
     int _ID;
     
+    /// @brief True if we use the joint mode
+    bool _mode;
+    
+    /// @brief True if all instructions must be done at the moment
+    bool _writeInstant;
+    
 public:
-    explicit AX12(int ID = -1, QObject *parent = 0);
+    
+    /// @brief Default constructor must pass an initialized dynamixel object
+    /// if ID == -1 no action is done
+    explicit AX12(dynamixel &dxl, int ID = -1, QObject *parent = 0);
+    
+    /// @brief Default destructor
     ~AX12();
+    
+    /// @brief Returns the current load from -100% to 100%, 100% is ClockWise 
+    /// and -100% is CounterClockWise
+    double getCurrentLoad();
+    
+    /// @brief Returns the current position from 0º to 300º
+    double getCurrentPos();
+    
+    /// @brief Returns the current Temperature in Celsius
+    int getCurrentTemp();
+    
+    /// @brief Returns the current speed from -100% to 100%, 100% is ClockWise
+    /// and -100% is CounterClockWise
+    double getCurrentSpeed();
+    
+    double getCurrentVoltage();
+    
+    /// @brief To get the current ID
+    inline int getID() { return _ID; }
+    
+    
+    
+    /// @brief To set a new ID
+    void setID(int ID);
+    
+    /// @brief To set Joint/Wheel mode, true if Joint
+    void setJointMode(bool mode);
+    
+    /// @brief To set the minimum and maximum angle from 0 to 300º
+    void setMinMax(double min, double max);
+    
+    /// @brief To set if a instruction must be done at the moment or later
+    /// <em>TRUE</em> to do instantly and <em>FALSE</em> to don't.
+    inline void setWriteInstant(bool inst) { _writeInstant = inst; }
+    
     
 signals:
     
