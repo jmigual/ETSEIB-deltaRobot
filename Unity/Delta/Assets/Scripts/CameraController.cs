136a1dd270cee1c8e90d8611486d379c7ebf0f9a
ImInSpace <ignigomoreno@hotmail.com>
2015-03-10 13:55:47 +0100

    Translacio

--- Unity/Delta/Assets/Scripts/CameraController.cs
+++ Unity/Delta/Assets/Scripts/CameraController.cs
@@ -14,7 +14,8 @@ public class CameraController : MonoBehaviour {
 	public float camRayLength=100f;
 	public float objectiveSwitchSpeed=10f;
 	public float dragSpeed=10f;
-	public Color selectedColor=Color.red;
+	public float movementSpeed=20f;
+	public float size = 5f;
 
 	
 	private Transform ObjectiveInitial;
@@ -22,38 +23,23 @@ public class CameraController : MonoBehaviour {
 	private const float rad=2*Mathf.PI/360;
 	private Vector3 ObjectiveDesired;
 	private Vector2 LastMouse;
-	private GameObject selected;
-	private Color lastColor;
 	private bool switchingobjective=false;
+	private bool centered=true;
+	private float desiredDistance;
 
 	void Start () {
+		desiredDistance=distance;
 		ObjectiveInitial=center.transform;
-		selected=ObjectiveInitial.gameObject;
-		lastColor=selected.renderer.material.color;
-		selected.renderer.material.color=selectedColor;
 		obj_pos = ObjectiveInitial.position;
 		ObjectiveDesired=obj_pos;
 	}
 
 	void Update () {
-		update_selected ();
 		update_objective ();
 		update_angles ();
 		update_transform();
 	}
-	void update_selected (){
-		if (Input.GetKeyDown (KeyCode.Mouse0)){
-			Ray CamRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-			Debug.DrawRay (CamRay.origin, CamRay.direction);
-			RaycastHit floorHit;
-			if (Physics.Raycast (CamRay, out floorHit, camRayLength, 1)) {
-				selected.renderer.material.color=lastColor;
-				selected=floorHit.collider.gameObject;
-				lastColor=selected.renderer.material.color;
-				selected.renderer.material.color = selectedColor;
-			}
-		}
-	}
+
 
 	void update_objective (){
 		if (Input.GetKeyDown (KeyCode.Mouse2)){
@@ -62,28 +48,67 @@ public class CameraController : MonoBehaviour {
 			RaycastHit floorHit;
 			if (Physics.Raycast (CamRay, out floorHit, camRayLength, 1)) {
 				switchingobjective=true;
+				centered=true;
 				center=floorHit.collider.gameObject;
 			}
 		}
-		ObjectiveDesired=center.transform.position;
+		if (Input.GetKey(KeyCode.A)){
+			if (centered){
+				centered=false;
+				switchingobjective=false;
+				ObjectiveDesired=obj_pos;
+			}
+			Vector3 desp=new Vector3 (Mathf.Sin(theta*rad),0,-Mathf.Cos(theta*rad));
+			obj_pos+=desp*movementSpeed*Time.fixedDeltaTime;
+		} 
+		if (Input.GetKey(KeyCode.D)){
+			if (centered){
+				centered=false;
+				switchingobjective=false;
+				ObjectiveDesired=obj_pos;
+			}
+			Vector3 desp=new Vector3 (-Mathf.Sin(theta*rad),0,Mathf.Cos(theta*rad));
+			obj_pos+=desp*movementSpeed*Time.fixedDeltaTime;
+		} 
+		if (Input.GetKey(KeyCode.W)){
+			if (centered){
+				centered=false;
+				switchingobjective=false;
+				ObjectiveDesired=obj_pos;
+			}
+			Vector3 desp=new Vector3 (-Mathf.Cos(theta*rad),0,-Mathf.Sin(theta*rad));
+			obj_pos+=desp*movementSpeed*Time.fixedDeltaTime;
+		} 
+		if (Input.GetKey(KeyCode.S)){
+			if (centered){
+				centered=false;
+				switchingobjective=false;
+				ObjectiveDesired=obj_pos;
+			}
+			Vector3 desp=new Vector3 (Mathf.Cos(theta*rad),0,Mathf.Sin(theta*rad));
+			obj_pos+=desp*movementSpeed*Time.fixedDeltaTime;
+		} 
+		if (centered)ObjectiveDesired=center.transform.position;
 		if (switchingobjective){
 			Vector3 error=ObjectiveDesired-obj_pos;
-			obj_pos=Vector3.Lerp(obj_pos, ObjectiveDesired, objectiveSwitchSpeed*Time.deltaTime/(Mathf.Pow (error.magnitude,0.5f)));
+			obj_pos=Vector3.Lerp(obj_pos, ObjectiveDesired, objectiveSwitchSpeed*Time.fixedDeltaTime/(Mathf.Pow (error.magnitude,0.5f)));
 			
-			Debug.Log(error.magnitude);
+			//Debug.Log(error.magnitude);
 			if (error.magnitude<0.02)switchingobjective=false;
 		}
 		else{
-			Debug.Log(0);
-			obj_pos=ObjectiveDesired;
+			if (centered)obj_pos=ObjectiveDesired;
+			Debug.DrawLine (transform.position, obj_pos);
 		}
 	}
 	void update_angles (){
-		distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*zoomspeed*Time.deltaTime, distanceMin, distanceMax);
-		if (Input.GetKey(KeyCode.A)) theta-=cameraspeed*Time.deltaTime;
-		if (Input.GetKey(KeyCode.D)) theta+=cameraspeed*Time.deltaTime;
-		if (Input.GetKey(KeyCode.W)) phi-=cameraspeed*Time.deltaTime;
-		if (Input.GetKey(KeyCode.S)) phi+=cameraspeed*Time.deltaTime;
+		//Debug.Log (desiredDistance);
+		//distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel")*zoomspeed*Time.fixedDeltaTime, distanceMin, distanceMax);
+		desiredDistance = Mathf.Clamp(desiredDistance - Input.GetAxis("Mouse ScrollWheel")*zoomspeed*Time.fixedDeltaTime, distanceMin, distanceMax);
+		distance=Mathf.Lerp (distance,desiredDistance,10*Time.fixedDeltaTime);
+		size -= zoomspeed*Time.fixedDeltaTime*Input.GetAxis("Mouse ScrollWheel");
+		size = Mathf.Clamp(size,2f,10f);
+		Camera.main.orthographicSize=size;
 		if (Input.GetKeyDown (KeyCode.Mouse1)) LastMouse=Input.mousePosition;
 		if (Input.GetKey (KeyCode.Mouse1)){
 			Vector2 dist = Input.mousePosition;
---
+++ Unity/Delta/Assets/Scripts/color.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class color : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay (transform.position,transform.forward);
+	}
+}
