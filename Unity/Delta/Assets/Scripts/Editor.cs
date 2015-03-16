98d538c633aa1ae35633231c8fde715974d91761
ImInSpace <ignigomoreno@hotmail.com>
2015-03-16 20:46:42 +0100

    Finixed Rotation + Deleting useless assets

--- Unity/Delta/Assets/Scripts/Editor.cs
+++ Unity/Delta/Assets/Scripts/Editor.cs
@@ -6,19 +6,21 @@ public class Editor : MonoBehaviour {
 	
 	public Color selectedColor = Color.red;
 	public GameObject X,Y,Z;
-	public GameObject RX, cruz;
+	public GameObject RX, RY, RZ;
 	public float arrowlength = 10f;
 	public float translateSpeed = 25f;
 	public string mode = "t";
+	public bool relativeRotation = true;
 
 	private GameObject selected;
 	private float camRayLength;
 	private Color lastColor;
 	private bool dragging=false;
-	private GameObject draggingAxis;
+	private GameObject draggingAxis,draggingRotator;
 	private Vector2 LastMouse;
-	private Vector3 toLastMouseRX;
+	private Vector3 toLastMouseR;
 	private string lastMode;
+	private Quaternion RXinitial, RYinitial, RZinitial;
 
 	// Use this for initialization
 	void Start () {
@@ -27,65 +29,62 @@ public class Editor : MonoBehaviour {
 		camRayLength = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>().camRayLength;
 		lastColor = selected.renderer.material.color;
 		selected.renderer.material.color = selectedColor;
+		if (relativeRotation){
+			RXinitial=RX.transform.rotation;
+			RYinitial=RY.transform.rotation;
+			RZinitial=RZ.transform.rotation;
+		}
 		X.GetComponentInChildren<Renderer>().material.color = Color.red;
 		Y.GetComponentInChildren<Renderer>().material.color = Color.green;
 		Z.GetComponentInChildren<Renderer>().material.color = Color.blue;
 		RX.GetComponentInChildren<Renderer>().material.color = Color.red;
-
-		Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		float t=Vector3.Dot (RX.transform.position,RX.transform.forward)-Vector3.Dot (RX.transform.forward,MouseRay.origin);
-		t=t/Vector3.Dot (RX.transform.forward,MouseRay.direction);
-		toLastMouseRX=MouseRay.origin+t*MouseRay.direction-RX.transform.position;
-		//RY.GetComponentInChildren<Renderer>().material.color = Color.green;
-		//RZ.GetComponentInChildren<Renderer>().material.color = Color.blue;
+		RY.GetComponentInChildren<Renderer>().material.color = Color.green;
+		RZ.GetComponentInChildren<Renderer>().material.color = Color.blue;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		float t=Vector3.Dot (RX.transform.position,RX.transform.forward)-Vector3.Dot (RX.transform.forward,MouseRay.origin);
-		t=t/Vector3.Dot (RX.transform.forward,MouseRay.direction);
-		Vector3 toPos=MouseRay.origin+t*MouseRay.direction-RX.transform.position;
-		float dAngle = Vector3.Angle(toLastMouseRX , toPos);
-		if (Vector3.Dot (RX.transform.forward,Vector3.Cross (toLastMouseRX,toPos))<0)dAngle=-dAngle;
-		toLastMouseRX=toPos;
-		cruz.transform.Rotate(RX.transform.forward*dAngle,Space.World);
+
 		//bola.transform.position=pos;
 		//Debug.DrawRay (RX.transform.position, Vector3.Cross(RX.transform.forward,Vector3.up).normalized*4);
 
+
+		align_tools ();
+		update_mode ();
+		update_selected();
+		align_tools ();
+	}
+
+	void align_tools (){
 		X.SetActive(mode=="t");
 		Y.SetActive(mode=="t");
 		Z.SetActive(mode=="t");
-<<<<<<< HEAD
-		X.transform.position = selected.transform.position;
-		Y.transform.position = selected.transform.position;
-		Z.transform.position = selected.transform.position;
-=======
 		RX.SetActive(mode=="r");
+		RY.SetActive(mode=="r");
+		RZ.SetActive(mode=="r");	
 		X.transform.position = selected.transform.position;
 		Y.transform.position = selected.transform.position;
 		Z.transform.position = selected.transform.position;
 		RX.transform.position = selected.transform.position;
->>>>>>> parent of 1367875... NO
-		update_mode ();
-		update_selected();
-		X.transform.position = selected.transform.position;
-		Y.transform.position = selected.transform.position;
-		Z.transform.position = selected.transform.position;
-<<<<<<< HEAD
-=======
-		RX.transform.position = selected.transform.position;
->>>>>>> parent of 1367875... NO
+		RY.transform.position = selected.transform.position;
+		RZ.transform.position = selected.transform.position;
+		if (relativeRotation){
+			RX.transform.rotation = selected.transform.rotation*RXinitial;
+			RY.transform.rotation = selected.transform.rotation*RYinitial;
+			RZ.transform.rotation = selected.transform.rotation*RZinitial;
+		}
 	}
 	void update_mode (){
 		//Debug.Log (mode);
-		if (Input.GetKeyDown(KeyCode.T)){
-			if (mode=="t")mode="-";
-			else mode="t";
-		}
-		if (Input.GetKeyDown(KeyCode.R)){
-			if (mode=="r")mode="-";
-			else mode="r";
+		if (Time.timeScale==0){
+			if (Input.GetKeyDown(KeyCode.T)){
+				if (mode=="t")mode="-";
+				else mode="t";
+			}
+			if (Input.GetKeyDown(KeyCode.R)){
+				if (mode=="r")mode="-";
+				else mode="r";
+			}
 		}
 		/*if (Input.GetKeyDown(KeyCode.X)){
 			Destroy (selected);
@@ -107,17 +106,18 @@ public class Editor : MonoBehaviour {
 					if (auxc == 'Y')draggingAxis = Y;
 					if (auxc == 'Z')draggingAxis = Z;
 				}
-<<<<<<< HEAD
-=======
-				if (floorHit.collider.gameObject.CompareTag("Rotator")){
-					LastMouse = Input.mousePosition;
+				else if (floorHit.collider.gameObject.CompareTag("Rotator")){
+					Debug.Log ("Hola");
 					dragging = true;
 					char auxc = floorHit.collider.gameObject.name[0];
-					if (auxc == 'X')draggingAxis = X;
-					if (auxc == 'Y')draggingAxis = Y;
-					if (auxc == 'Z')draggingAxis = Z;
+					if (auxc == 'X')draggingRotator = RX;
+					if (auxc == 'Y')draggingRotator = RY;
+					if (auxc == 'Z')draggingRotator = RZ;
+					Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+					float t=Vector3.Dot (draggingRotator.transform.position,draggingRotator.transform.forward)-Vector3.Dot (draggingRotator.transform.forward,MouseRay.origin);
+					t=t/Vector3.Dot (draggingRotator.transform.forward,MouseRay.direction);
+					toLastMouseR=MouseRay.origin+t*MouseRay.direction-draggingRotator.transform.position;
 				}
->>>>>>> parent of 1367875... NO
 				else{
 					selected.renderer.material.color = lastColor;
 					selected = floorHit.collider.gameObject;
@@ -142,12 +142,21 @@ public class Editor : MonoBehaviour {
 			Debug.Log (axisScreen);*/
 			float res = Vector2.Dot (despMouse,axisScreen)/Mathf.Pow(axisScreen.magnitude,2);
 			selected.transform.position+=draggingAxis.transform.up*res*translateSpeed;
-
+		}
+		if (Input.GetKey (KeyCode.Mouse0) && dragging && mode=="r"){
+			Ray MouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float t=Vector3.Dot (draggingRotator.transform.position,draggingRotator.transform.forward)-Vector3.Dot (draggingRotator.transform.forward,MouseRay.origin);
+			t=t/Vector3.Dot (draggingRotator.transform.forward,MouseRay.direction);
+			Vector3 toPos=MouseRay.origin+t*MouseRay.direction-draggingRotator.transform.position;
+			float dAngle = Vector3.Angle(toLastMouseR , toPos);
+			if (Vector3.Dot (draggingRotator.transform.forward,Vector3.Cross (toLastMouseR,toPos))<0)dAngle=-dAngle;
+			toLastMouseR=toPos;
+			selected.transform.Rotate(draggingRotator.transform.forward*dAngle,Space.World);
 		}
 	}
 
 	public void PausePlay (){
-		Debug.Log (Time.timeScale);
+		//Debug.Log (Time.timeScale);
 		if (Time.timeScale == 1) {
 						Time.timeScale = 0;
 						GameObject.FindGameObjectWithTag ("Pause").GetComponentInChildren <Text> ().text = "Play";
