8d92ff255b41d82e1193735d2df0eba0dce546d1
Kittus <markasenjo@hotmail.com>
2015-04-13 22:23:59 +0200

    a ver

---
+++ Unity/Delta/Assets/Scripts/dominoPosition.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dominoPosition : MonoBehaviour {
+	public float height;
+	public bool forceApplyer;
+	public bool adding = false;
+	public float speed = 10f;
+
+
+	private bool shot = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (adding) {
+					Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+					RaycastHit floorHit;
+			
+					if (Physics.Raycast (camRay, out floorHit, 100f, LayerMask.GetMask ("Floor"))) {
+						transform.position = floorHit.point + height*Vector3.up;
+					}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Mouse0))
+						adding = false;
+
+		if (forceApplyer && !shot && Time.timeScale==Editor.timescale) {
+			rigidbody.velocity = transform.forward * speed;
+		}
+	}
+
+	void OnCollisionEnter (Collision collision){
+		if (forceApplyer && collision.gameObject.tag == "Player") {
+						gameObject.SetActive(false);
+						//Debug.Log ("lel");
+				}
+	}
+}
