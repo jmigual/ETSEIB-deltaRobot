e73a9d6622857222a351119192d41cdfb0588e3e
jmigual <j.marce.igual@gmail.com>
2015-05-22 21:10:38 +0200

    Added power instead of multiplications

--- Matlab/singleAngle.m
+++ Matlab/singleAngle.m
@@ -1,21 +1,25 @@
 function angle = singleAngle(x0, y0, z0, a, b)
 
-n = b*b - a*a - z0*z0 - x0*x0 - y0*y0;
-root = sqrt (n*n*y0*y0 - 4*(x0*x0 + y0*y0)*(-x0*x0*a*a + n*n/4));
+n = b^2 - a^2 - z0^2 - x0^2 - y0^2;
+root = sqrt (n^2*y0^2 - 4*(x0*^2 + y0*^2)*(-x0*^2*a^2 + n^2/4));
 
 if (x0 < 0)
     root = root * -1;
 end
 
-y = (-n*y0 + root ) / (2*(x0*x0 + y0*y0));
+y = (-n*y0 + root ) / (2*(x0^2 + y0^2));
 sign = 1;
 
-if ((b*b - (y0 + a)*(y0 + a)) < (x0*x0 + z0*z0) && x0 < 0)
+if ((b^2 - (y0 + a)*(y0 + a)) < (x0^2 + z0^2) && x0 < 0)
     sign = -1;
 end
 
-x = sqrt(a*a - y*y)*sign;
+x = sqrt(a^2 - y^2)*sign;
 
-angle = atan2(y, x);
+if (isreal(x) && isreal(y))
+    angle = atan2(y, x);
+else
+    angle = NaN;
+end
 
 end
